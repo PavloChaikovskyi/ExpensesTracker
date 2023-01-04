c477b237a28545398d7a01d870e1563a18930471
PavloChaikovskyi <chaykovskiypavel@gmail.com>
2023-01-04 08:55:02 +0100

    added styles to component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import ExpenseItem  from "./components/ExpenseItem";
+
 function App() {
   return (
-
+    
       <div className="App-header">
         <h1>Hello World</h1>
         <p>Paragraph</p>
+        <ExpenseItem></ExpenseItem>
       </div>
-
+      
   );
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return <h2>Expense item!</h2>
+    return (
+      <div className='expense-item'>
+        <div>04 Jan 2023</div>
+        <div className='expense-item__description'>
+          <div>Car Insurance</div>
+          <div className='expense-item__price'>$200.00</div>
+        </div>
+      </div>
+    );
 }
 
-export default ExpenseItem;
+export default ExpenseItem;   
