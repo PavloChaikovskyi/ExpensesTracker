e5fe7ea8063fec2b92a7b0dadb4fb72ba528133f
PavloChaikovskyi <chaykovskiypavel@gmail.com>
2023-01-06 18:11:56 +0100

    update expenses arr by adding new expense from the form by setNewState

--- src/App.js
+++ src/App.js
@@ -1,47 +1,31 @@
 import Expenses from "./components/Expenses/Expenses";
-import React from 'react'
+import React, { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: "200.00",
+    date: new Date(2023, 1, 4),
+  }
+]; 
 
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: "200.00",
-      date: new Date(2023, 1, 4),
-    },
-    {
-      id: "e2",
-      title: "Car Insurance",
-      amount: "220.00",
-      date: new Date(2023, 2, 4),
-    },
-    {
-      id: "e3",
-      title: "Candies",
-      amount: "230.00",
-      date: new Date(2023, 2, 5),
-    },
-    {
-      id: "e4",
-      title: "Candies",
-      amount: "230.00",
-      date: new Date(2023, 2, 5),
-    },
-  ]; 
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
-  }
+  const addExpenseHandler = (expense) => {
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    });
+  };
 
   return (
     <div className="App-header">
-      <NewExpense  onAddExpense={addExpenseHandler}/>
-      <Expenses  items={expenses}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
