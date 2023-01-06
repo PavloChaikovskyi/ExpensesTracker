87096c765b260fd9aab1bd6f3235fdd380fc0dd0
PavloChaikovskyi <chaykovskiypavel@gmail.com>
2023-01-06 13:19:52 +0100

    push data up from NewExpense.js to App.js by props

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,20 +3,21 @@ import './NewExpense.css';
 import ExpenseForm from "./ExpenseForm";
 
 
-const NewExpense = () => {
+const NewExpense = (props) => {
     
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         }
-        console.log(expenseData);
+        // console.log(expenseData);
+        props.onAddExpense(expenseData);
     }
     
     return <div className="new-expense">
 
     <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-
+    
     </div>
 };
 
