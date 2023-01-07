3a5fcb764b230ca28ae5aa28c6a8ef847e5ec3b3
PavloChaikovskyi <chaykovskiypavel@gmail.com>
2023-01-07 22:26:11 +0100

    restruct Expenses.js and create ExpensesList.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,8 @@
 import "./Expenses.css"
-import ExpenseItem  from "./ExpenseItem";
 import Card from "./../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import React, {useState} from "react";
-
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
 
@@ -17,26 +16,13 @@ const filteredExpenses = filteredYear.toString() === 'All' ? props.items : props
   (item) => item.date.getFullYear().toString() === filteredYear
 );
 
-let expensesContent = <p>No expenses found</p>;  
-if (filteredExpenses.length > 0) {
-  expensesContent = filteredExpenses.map((expense) => {
-    return (
-      <ExpenseItem
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-        key={expense.id}
-      />
-    );
-  })}
-
 return (
   <Card className="expenses">
     <ExpensesFilter
       selected={filteredYear}
       onChangeFilter={filterChangeHandler}
     />
-    {expensesContent}
+    <ExpensesList items={filteredExpenses} />
   </Card>
 );
 }
